4c059d12d52db1314b4b909ddd9251c7f4f224fe
yvgk1 <emretopal32@gmail.com>
2023-03-15 01:48:57 +0300

    PythonHomework_3
    kodlama.io 2.gün ödev2

---
+++ classes.py
@@ -0,0 +1,11 @@
+class Banka:
+    def krediBasvur(self):
+        print("Kredi Basvurusu Yapıldı")
+
+    def krediHesapla(self):
+        print("Hesaplar Yapıldı")
+
+banka=Banka()
+banka.krediBasvur()
+
+# self=this
---
+++ person.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self,name,lastName):
+        self.name=name
+        self.lastName=lastName
+
+
+
+musteri1=Person("Ahmet","Demiroğ")
+musteri2=Person("Kerem","Demiroğ")
+musteri3=Person("İlker","Demiroğ")
+
+print(musteri1.name)
+print(musteri1.lastName)
